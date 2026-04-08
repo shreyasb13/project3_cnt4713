f7692a2e30d97ff62b5c2bf7255bae867decd4a5
shreyasb13 <shre.jsb@gmail.com>
2026-04-08 17:38:35 -0400

    Commit part 2/4 in the rubric

--- mycrypto.py
+++ mycrypto.py
@@ -5,29 +5,93 @@
 
 # Import socket module
 from socket import *
+# Import RSA Encryption and Decryption
+from Crypto.PublicKey import RSA
 import sys
 
+# Usage: python mycrypto.py server
+# Usage: python mycrypto.py client localhost 8080
 
-def sendCommand(clientSocket, command): #for the client side
+def sendCommand(clientSocket, command): # For the client side
     dataOut = command.encode("utf-8")
-    clientSocket.sendall(dataOut) #send to server
-    dataIn = clientSocket.recv(1024) #recieve the response in bytes
-    data = dataIn.decode("utf-8") #decode the data
-    return data #return the decoded data
+    clientSocket.sendall(dataOut) # Send to server
+    dataIn = clientSocket.recv(1024) # Receive the response in bytes
+    data = dataIn.decode("utf-8") # Decode the data
+    return data # Return the decoded data
 
-def receiveData(clientSocket): #for server side
+def receiveData(clientSocket): # For server side
     dataIn = clientSocket.recv(1024)
     data = dataIn.decode("utf-8")
     return data
 
+def server():
+    print("Starting server...")
+
+    # Part 2 of Rubric - Tunnel
+    print("Creating RSA keypair")
+    serverKey = RSA.generate(2048)
+    serverPubKey = serverKey.publickey()
+    print("RSA keypair created")
+
+    # Part 1 of Rubric - Connect
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('', 8080))
+    serverSocket.listen(1)
+    print("Awaiting connections...")
+    clientSocket, addr = serverSocket.accept()
+    print("Connection requested. Creating data socket")
+    dataSocket = socket(AF_INET, SOCK_STREAM)
+    dataSocket.bind(('', 0))
+    dataSocket.listen(1)
+    port = dataSocket.getsockname()[1]
+    clientSocket.sendall(str(port).encode("utf-8"))
+    clientSocket.close()
+    dataSocket, addr = dataSocket.accept()
+
+    # Part 2 of Rubric - Tunnel
+    print("Tunnel requested. Sending public key")
+    clientKeyData = receiveData(dataSocket)  # Receives tunnel command
+    clientPubKey = RSA.importKey(clientKeyData)
+    serverKeyData = serverPubKey.exportKey().decode("utf-8")
+    dataSocket.sendall(serverKeyData.encode("utf-8"))
+
+    # Part 3 of Rubric - Post
+
+
+def client():
+    print("Starting client...")
+
+    # Part 2 of Rubric - Tunnel
+    print("Creating RSA keypair")
+    clientKey = RSA.generate(2048)
+    clientPubKey = clientKey.publickey()
+    print("RSA keypair created")
+
+    # Part 1 of Rubric - Connect
+    print("Creating client socket")
+    clientSocket = socket(AF_INET, SOCK_STREAM)  # Create first socket
+    print("Connecting to server")
+    clientSocket.connect((sys.argv[2], int(sys.argv[3])))  # Connect socket to the server with the IP and port
+    port = sendCommand(clientSocket, "connect")  # Send the command and receive the port
+    print("Creating data socket")
+    clientSocket2 = socket(AF_INET, SOCK_STREAM)  # Create second socket
+    clientSocket2.connect((sys.argv[2], int(port)))  # Connect second socket to the new port using the same IP
+
+    # Part 2 of Rubric - Tunnel
+    print("Requesting tunnel")
+    clientKeyData = clientPubKey.exportKey().decode("utf-8")
+    serverKeyData = sendCommand(clientSocket2, clientKeyData)
+    serverPubKey = RSA.importKey(serverKeyData)
+    print("Server public key received")
+    print("Tunnel established")
+
+    # Part 3 of Rubric - Post
+
 def main():
-    #part 1 of rubric
-    clientSocket = socket(AF_INET, SOCK_STREAM) #Create first socket
-    clientSocket.connect((sys.argv[1], int(sys.argv[2]))) #connect socket to the server with the IP and port
-    port = sendCommand(clientSocket, "connect") #send the command and receive the port 
-    print('Port return: ', port) #print port
-    clientSocket2 = socket(AF_INET, SOCK_STREAM) #create second socket
-    clientSocket2.connect((sys.argv[1], int(port))) #connect second socket to the new port using hte same IP
+    if sys.argv[1] == "server":
+        server()
+    elif sys.argv[1] == "client":
+        client()
 
 if __name__ == "__main__":
     main()
